e5e502f073b0aa09500e1150470f15b26362eca1
mscal <mscaljrokline@gmail.com>
2024-02-26 12:13:16 -0400

    rxjs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,20 @@
 import { Component } from '@angular/core';
 import { initFlowbite } from 'flowbite';
 
+
+import { of } from 'rxjs';
+import { filter } from 'rxjs/operators';
+
+const dataSource = of(1, 2, 3, 4, 5);
+
+// subscribe to our source observable
+const subscription = dataSource
+  .pipe(
+    // only accept values 2 or greater
+    filter(value => value >= 2)
+  )
+  // log: 2, 3, 4, 5
+  .subscribe(value => console.log(value));
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
